34e64af9b560dc8c0482e4f1c953a2677b6b683c
Edwin Fame <edwinfame@yahoo.com>
2022-03-11 13:41:16 -0800

    Added js functionality

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,15 @@
 // define all DOM elements to be used
-var currentDayEl = $('#currentDay');
+const currentDayEl = $('#currentDay');
 
+//
+$(function(){
 
-
+  //Save button functionality saves the task and the hour of the task to local storage.
+  $('.saveBTn').on('click', function(){
+    let task = $(this).siblings('.task').val();
+    let time = $(this).parent().attr('id');
+    localStorage.setItem(task, time);
+  });
 
 
 
@@ -13,5 +20,22 @@ function displayDate() {
     currentDayEl.text(rightNow);
   }
 
+ function Init() {
+   $('#hour9 .task').val(localStorage.getItem('hour9'));
+   $('#hour10 .task').val(localStorage.getItem('hour10'));
+   $('#hour11 .task').val(localStorage.getItem('hour11'));
+   $('#hour12 .task').val(localStorage.getItem('hour12'));
+   $('#hour13 .task').val(localStorage.getItem('hour13'));
+   $('#hour14 .task').val(localStorage.getItem('hour14'));
+   $('#hour15 .task').val(localStorage.getItem('hour15'));
+   $('#hour16 .task').val(localStorage.getItem('hour16'));
+   $('#hour17 .task').val(localStorage.getItem('hour17'));
+   displayDate()
+ }
+
+ Init()
 
  setInterval(displayDate, 1000);
+})
+
+ 
